caa03ed815922ccab6b4d13b788536582d4737b4
Martin Schmidt <martin.schmidt@htw-dresden.de>
2019-01-07 18:05:56 +0100

    Tasten_RGB-LED

---
+++ Tasten_RGB-LED/Tasten_RGB-LED.js
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, () => {
+    basic.setLedColor(Colors.Red)
+})
+input.onButtonPressed(Button.B, () => {
+    basic.setLedColor(Colors.Blue)
+})
+input.onButtonPressed(Button.AB, () => {
+    basic.setLedColor(Colors.Purple)
+})
+
